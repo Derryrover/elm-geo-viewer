bd3d31b445ee50c5060a02a8bd1633ef3e483642
derryrover <derryrover@github.com>
2019-11-19 21:37:12 +0100

    simplify layer

--- src/Map/MapLayer.elm
+++ src/Map/MapLayer.elm
@@ -1,16 +1,22 @@
 module MapLayer exposing(..)
 
 import Html.Attributes exposing (style, class,value, src, alt, id)
-import Browser exposing(element)
 import Html exposing (..)
-import Html.Keyed exposing(node)
+import Html.Keyed
+import List
 
 import ElmStyle
 import Types
 
-keyedDiv = node "div"
+keyedDiv = Html.Keyed.node "div"
 
-mapLayer model tileUrl = 
+createKey x y = "keyed_str_x_y_"++(String.fromInt x) ++ "_"++(String.fromInt y)
+
+flatten2D : List (List a) -> List a
+flatten2D list =
+  List.foldr (++) [] list
+
+mapLayer model createTileUrl = 
   let
     maxTilesOnAxis = Types.tilesFromZoom model.map.zoom
   in
@@ -22,34 +28,47 @@ mapLayer model tileUrl =
             , ("pointer-events", "none")
             ] 
           )
-          ( List.map (\y ->
-            ( ("keyed_div_y_value_"++(String.fromInt y)) 
-            , keyedDiv
-              ( ElmStyle.createStyleList 
-                  [ ("height", ElmStyle.intToPxString Types.tilePixelSize)
-                  , ("position", "absolute")
-                  , ("top", ElmStyle.intToPxString (Types.tilePixelSize * y))
-                  ] 
-              )
-              (List.map (\x ->
-                ( ("keyed_div_x_y_value_"++(String.fromInt x) ++ "_"++(String.fromInt y)) 
-                , div
-                ( ElmStyle.createStyleList 
-                          [ ("height", ElmStyle.intToPxString Types.tilePixelSize)
-                          , ("width", ElmStyle.intToPxString Types.tilePixelSize)
-                          , ("position", "absolute")
-                          , ("left", ElmStyle.intToPxString (Types.tilePixelSize * x)) ])
-                [ img
-                  (List.concat [ 
-                    [ (src (tileUrl model.map.zoom (modBy maxTilesOnAxis x) (modBy maxTilesOnAxis y)))]
-                  , ( ElmStyle.createStyleList 
-                      [ ("height", "100%")
-                      , ("width", "100%") ]
-                    )])
-                  [] ]
-                )) 
-                model.map.tileRange.rangeX
-              )
-          ))
-          model.map.tileRange.rangeY
-        )
+           (flatten2D 
+            ( List.map (\y ->
+                List.map (\x ->
+                  ( createKey x y 
+                  , imageDiv model createTileUrl x y 
+                  )) 
+                  model.map.tileRange.rangeX
+                )
+              model.map.tileRange.rangeY
+            ))
+
+imageDiv model createTileUrl x y = 
+  let
+    maxTilesOnAxis = Types.tilesFromZoom model.map.zoom
+    xMod = modBy maxTilesOnAxis x
+    yMod = modBy maxTilesOnAxis y
+    url = createTileUrl model.map.zoom xMod yMod
+  in
+    div
+      ( ElmStyle.createStyleList 
+                [ ("position", "absolute")
+                , ("top", ElmStyle.intToPxString (Types.tilePixelSize * y))
+                , ("left", ElmStyle.intToPxString (Types.tilePixelSize * x)) 
+                ])
+      [ img
+        [ src url]
+        []]
+
+
+-- [ ("position", "absolute")
+-- , ("top", ElmStyle.intToPxString (Types.tilePixelSize * y))
+-- , ("left", ElmStyle.intToPxString (Types.tilePixelSize * x)) 
+-- , ("height", ElmStyle.intToPxString Types.tilePixelSize)
+-- , ("width", ElmStyle.intToPxString Types.tilePixelSize)
+-- ])
+
+  -- [ img
+  --       (List.concat [ 
+  --         [ (src (tileUrl model.map.zoom (modBy maxTilesOnAxis x) (modBy maxTilesOnAxis y)))]
+  --       , ( ElmStyle.createStyleList 
+  --           [ ("height", "100%")
+  --           , ("width", "100%") ]
+  --         )])
+  --       [] ]
